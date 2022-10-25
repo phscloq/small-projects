6c360d0e4214341fce44f7ec6fcdb97ec490b53e
Baran Dogan <phscloq@gmail.com>
2022-10-25 14:11:46 +0300

    The bank app file added into the parent file and also a few changes in calculator's script file.

--- calculator/script.js
+++ calculator/script.js
@@ -2,15 +2,3 @@ console.log('Hello from script.js');
 const display = document.querySelector('.display');
 const buttons = document.querySelectorAll('button');
 
-// Add event listener to each button
-buttons.forEach(button => {
-  button.addEventListener('click', calculate);
-  console.log("basuıldı");
-});
-
-// Calculate function
-function calculate(event) {
-  console.log(event.target.value);
-  console.log("basuıldı");
-    const clickedButtonValue = event.target.value;
-}
