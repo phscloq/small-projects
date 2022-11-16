2901b00c22412adf59ab1cfa94aa26a2cc624e02
Baran Dogan <phscloq@gmail.com>
2022-11-16 11:43:04 +0300

    fixed some bugs

--- library-project/script.js
+++ library-project/script.js
@@ -4,12 +4,13 @@ function Book(title, author, pages, read) {
             this.author = author;
             this.pages = pages;
             this.read = read;
-                
+      
         }
 
         function displayBooks(){
         let bookCard = document.createElement('div');
         bookCard.className = "book-card";
+        bookCard.id = "";
         let bookContent = document.createElement('div');
         bookContent.className = "book-card__content";
         let bookTitle = document.createElement('h2');
@@ -20,66 +21,80 @@ function Book(title, author, pages, read) {
         bookPages.className= "book-card__pages";
         let bookStatus = document.createElement('p');
         bookStatus.className= "book-card__read";
+     
         let deleteButton = document.createElement('button');
         deleteButton.className="book-card__button";
+        deleteButton.onclick = deleteBook;
         deleteButton.innerHTML="Del";
         let title = document.getElementById("title").value;
         let author = document.getElementById('author').value;
         let pages = document.getElementById('pages').value;
         let read = document.getElementById('read').value;
+    //change the color of the book card depending on the read status
         if(document.getElementById('read').checked){
             read = "Read";
+            bookStatus.style.color = "black";
         }else{
             read = "Not Read";
+            bookStatus.style.color = "red";
         }
+        
+        
 
-
-
-        let book = new Book(title, author, pages, read);
+        if(title.length>0 && author.length>0 && pages.length>0){
+            let book = new Book(title, author, pages, read);
         myLibrary.push(book);
-            
+        console.log(myLibrary);
+        if(myLibrary.length ==14){
+        
+            let button = document.getElementById('submitbut');
+            button.disabled = true;
+            let warning = document.getElementsByClassName('warning');
+            warning[0].style.display = "block";
+          
+        }    
         bookTitle.textContent = myLibrary[myLibrary.length - 1].title;
         bookAuthor.textContent = myLibrary[myLibrary.length - 1].author;
         bookPages.textContent = myLibrary[myLibrary.length - 1].pages;
         bookStatus.textContent = myLibrary[myLibrary.length - 1].read;
+  
 
+        bookCard.id = myLibrary.length-1;
         bookContent.appendChild(bookTitle);
         bookContent.appendChild(bookAuthor);
         bookContent.appendChild(bookPages);
         bookContent.appendChild(bookStatus);
         bookContent.appendChild(deleteButton);
+
         bookCard.appendChild(bookContent);
             
         let content = document.getElementsByClassName('content');
         content[0].appendChild(bookCard);
-        if(myLibrary.length >=14){
-            alert("You have reached the maximum number of books");
-            let button = document.getElementById('submitbut');
-            button.disabled = true;
+        }else if(title.length==0 || author.length==0 || pages.length==0){
+            alert("Please fill all the required information about the book");
         }
-        }
-         
 
+
+
+        
+        }
+       
         function deleteBook(){
-            let bookCard = document.getElementsByClassName('book-card');
-            bookCard[0].remove();
+            let bookId = this.parentNode.parentNode.id;
+            let bookCard = this.parentNode.parentNode;
+            let content = document.getElementsByClassName('content');
+            content[0].removeChild(bookCard);
+            myLibrary.splice(bookId, 1);
+            console.log(myLibrary);
+            if(myLibrary.length<14){
+                let button = document.getElementById('submitbut');
+                button.disabled = false;
+            } 
         }
         
 
 
- 
 
-/* function addBookToLibrary(){
-    console.log("Clicked submit");
-                let title = document.getElementById("title").value;
-                let author = document.getElementById('author').value;
-                let pages = document.getElementById('pages').value;
-                let read = document.getElementById('read').value;
-                let book = new Book(title, author, pages, read);
-                myLibrary.push(book);
-              
-                return myLibrary[myLibrary.length - 1];
-            } */
            
             function formDisplay(){
                 let form = document.getElementsByClassName('form');
